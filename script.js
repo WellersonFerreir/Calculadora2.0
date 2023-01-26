dc65278fd1afd44151b7305d1888db27f831af2f
Wellerson Ferreira <117539408+WellersonFerreir@users.noreply.github.com>
2023-01-26 20:47:06 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let buttons = document.querySelectorAll('.number');
-
-for(var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function(){
-        var num = this.textContent;
-        display.textContent = num;
-      });
-}
